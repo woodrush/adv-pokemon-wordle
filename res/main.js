852f5aa2c35faf90b4f7ecb332d8f6d2120accc2
Hikaru Ikuta <woodrush924@gmail.com>
2022-02-03 23:34:33 +0900

    Keyboard input

--- res/main.js
+++ res/main.js
@@ -1,4 +1,38 @@
-function onClickKey(key_dom){
-    let keychar = key_dom.getAttribute("key");
+var currow = 0;
+var curcol = 0;
+var str = "";
+
+function onClickKey(key_dom) {
+    const keychar = key_dom.getAttribute("key");
     console.log(keychar);
+    typekey(keychar);
+}
+
+function showbox(s) {
+    for(let i=0; i<5; i++){
+        const box = document.getElementById("box-" + currow + "-" + i);
+        if (s[i]) {
+            box.innerHTML = s[i];
+        } else {
+            box.innerHTML = "";
+        }
+    }
+}
+
+function typekey(key) {
+    if (key == "bs") {
+        str = str.slice(0,curcol-1);
+        curcol -= 1;
+    } else if (curcol > 4) {
+        return;
+    } else {
+        str = str + key;
+        curcol += 1;
+    }
+    showbox(str);
+    // const rows = board.querySelectorAll("tr");
+    // const lastrow = rows[rows.length-1];
+
+    // const cols = lastrow.querySelectorAll("td");
+    // const nextcol = cols[cols.length-1];
 }
